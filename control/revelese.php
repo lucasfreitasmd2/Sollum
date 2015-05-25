fea96de007f669dc9cf51ef22d7777f1cead86b7
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-05-25 16:45:20 -0300

    email de perfil para aprovação

--- control/revelese.php
+++ control/revelese.php
@@ -29,8 +29,24 @@ if ($nome != '' && $email != '' && $telefone != '' && $cidade != '' && $estado !
 
         $objReveleseDao->cadArquiteto($objRevelese);
     }
-    
+
     print_r($retorno);
+
+    //enviar email
+    $mensagem = $nome.' Enviou solicitou uma aprovação para se revelar no site';
+    $para = $email;
+    $subjectText = "Revele-se | REVESTIMENTOS E ACABAMENTOS";
+    $msgText = "<font size='2' face='verdana'>";
+    $msgText .= "Nome: " . $nome . "<br>";
+    $msgText .= "E-Mail: " . $mail . "<br>";
+    $msgText .= "Telefone: " . $telefone . "<br>";
+    $msgText .= "Mensagem: " . $mensagem . "</font>";
+
+    if ($nome == '' && $mail == '' && $telefone = '' && $mensagem == "") {
+        header('Location: contato.php');
+    } else {
+        mail($para, $subjectText, $msgText, "From: $nome - $email\n" . "MIME-Version: 1.0\n" . "Content-type: text/html; charset=utf-8");
+    }
 } else {
     header('Location: ../');
 }
