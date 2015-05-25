e73ae60d0e5e11e0571b602f61bad4626a080ea2
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-05-25 15:09:50 -0300

    validação do campo de telefone

--- js/revele-se.js
+++ js/revele-se.js
@@ -1,6 +1,43 @@
+function validaEmail(email) {
+    er = /^[a-zA-Z0-9][a-zA-Z0-9\._-]+@([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2}/;
+    if (er.exec(email))
+        return true;
+    else
+        return false;
+}
+
+function validaTelefone(telefone) {
+    var er = /^[0-9]/;
+    var valido = '';
+    if (er.exec(telefone)) {
+        valido = true;
+    } else {
+        valido = false;
+    }
+
+    if (valido === false || telefone.length > 11 || telefone.length < 10) {
+        valido = false;
+    }else{
+        valido = true;
+    }
+    
+    return valido;
+}
+
 $(document).ready(function () {
     $("input[type='text'], textarea").blur(function () {
         $('#spanError').fadeOut(500);
+    });
+
+    $("#telefone").blur(function () {
+        var telefone = $("#telefone").val().trim();
+
+        if (validaTelefone(telefone) === false) {
+            $("#telefone").focus();
+            $("#spanError").html('');
+            $("#spanError").html('O TELEFONE que você digitou não é válido');
+            $('#spanError').fadeIn('slow');
+        }
     });
 
     $("#btnRevelese").click(function () {
@@ -22,10 +59,20 @@ $(document).ready(function () {
             $("#spanError").html('');
             $("#spanError").html('O campo EMAIL é obrigatório!');
             $('#spanError').fadeIn('slow');
+        } else if (validaEmail(email) === false) {
+            $("#email").focus();
+            $("#spanError").html('');
+            $("#spanError").html('O E-MAIL que você digitou não é válido!');
+            $('#spanError').fadeIn('slow');
         } else if (telefone == '') {
             $("#telefone").focus();
             $("#spanError").html('');
             $("#spanError").html('O campo TELEFONE é obrigatório!');
+            $('#spanError').fadeIn('slow');
+        } else if (validaTelefone(telefone) === false) {
+            $("#telefone").focus();
+            $("#spanError").html('');
+            $("#spanError").html('O TELEFONE que você digitou não é válido');
             $('#spanError').fadeIn('slow');
         } else if (cidade == '') {
             $("#cidade").focus();
@@ -42,16 +89,16 @@ $(document).ready(function () {
             $("#spanError").html('');
             $("#spanError").html('O campo CURRÍCULO é obrigatório!');
             $('#spanError').fadeIn('slow');
-        }else{
-            $.post('control/revelese.php',{nome:nome, email:email,telefone:telefone,cidade:cidade,estado:estado,curriculo:curriculo},
-            function(r){
-                if(r == 0){
+        } else {
+            $.post('control/revelese.php', {nome: nome, email: email, telefone: telefone, cidade: cidade, estado: estado, curriculo: curriculo},
+            function (r) {
+                if (r == 0) {
                     $("#spanError").html('');
                     $("#spanError").html('Este usuário já se encontra cadastrado em nossa base de dados!');
                     $('#spanError').fadeIn('slow');
                 }
             });
-            
+
         }
     }
     );
