ce352360ccd6ec90645cac2fc75b8665ed444265
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-05-25 11:51:52 -0300

    query local

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 <link rel="stylesheet" type="text/css" href="css/style.css"/>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> 
+<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>--> 
+<script src="js/jquery.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
         $('nav.menu ul').css('display', 'none');
